0762235338f80379764fdc8a8b03da586ea8293f
Piko327 <fikii.business@gmail.com>
2023-11-13 20:45:49 +0200

    add animation on blogs page

--- bellebloom/src/app/components/Blogs/Blogs.jsx
+++ bellebloom/src/app/components/Blogs/Blogs.jsx
@@ -3,17 +3,22 @@ import React from "react";
 import style from "./style.scss";
 import Image from "next/image";
 import ShowEffectProvider from "@/app/providers/ShowEffectProvider";
+import { motion } from "framer-motion";
 
 const Blogs = ({ props }) => {
-  const framerVariant = {
-    hidden: { x: -300, opacity: 0 },
+  const container = {
+    hidden: { opacity: 0 },
     show: {
       opacity: 1,
-      x: 0,
       transition: {
-        duration: 1,
+        staggerChildren: 0.5,
       },
     },
+  };
+
+  const item = {
+    hidden: { opacity: 0 },
+    show: { opacity: 1 },
   };
 
   const framerVariantTitle = {
@@ -22,32 +27,36 @@ const Blogs = ({ props }) => {
       opacity: 1,
       y: 0,
       transition: {
-        duration: .8,
+        duration: 0.8,
       },
     },
   };
+  
   return (
     <section className="section-blogs">
       <div className="shell">
         <div className="section__inner">
-            <ShowEffectProvider variant={framerVariantTitle}>
-          <h2>{props[0].blogTitle}</h2>
+          <ShowEffectProvider variant={framerVariantTitle}>
+            <h2>{props[0].blogTitle}</h2>
           </ShowEffectProvider>
-          <ShowEffectProvider variant={framerVariant}>
-            <ul className="blogs">
-              {props.map((card) => (
-                <li key={card.id} className="blog">
-                  <Image width={400} height={400} src={card.image} alt="" />
+          <motion.ul
+            initial="hidden"
+            whileInView="show"
+            variants={container}
+            className="blogs"
+          >
+            {props.map((card) => (
+              <motion.li variants={item} key={card.id} className="blog">
+                <Image width={400} height={400} src={card.image} alt="" />
 
-                  <div className="card__text">
-                    <h3>{card.title}</h3>
+                <div className="card__text">
+                  <h3>{card.title}</h3>
 
-                    <a href="">More</a>
-                  </div>
-                </li>
-              ))}
-            </ul>
-          </ShowEffectProvider>
+                  <a href="">More</a>
+                </div>
+              </motion.li>
+            ))}
+          </motion.ul>
           <a href="" className="btn btn--reverse ">
             <span>More </span>
           </a>
