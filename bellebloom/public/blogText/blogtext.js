997f4a9f2ca3b8a683fac1ac43f29ba2e3bd887e
Piko327 <fikii.business@gmail.com>
2023-11-13 16:18:32 +0200

    Resized project images for improved performance and reduced file size

--- bellebloom/public/blogText/blogtext.js
+++ bellebloom/public/blogText/blogtext.js
@@ -1,72 +1,70 @@
 export const blogText = [
   [
-    { id: 1, 
-      blogTitle:'Face',
+    {
+      id: 1,
+      blogTitle: "Face",
       title: "Cleansers",
-       image: "/images/BlogImages/Cleanser 1.png" },
+      image: "/images/BlogImages/Cleanser 1.png",
+    },
     {
       id: 2,
-      blogTitle:'Face',
+      blogTitle: "Face",
       title: "EyePatches",
       image: "/images/BlogImages/patches-box.png",
     },
-    { id: 3,
-      blogTitle:'Face',
-       title: "Мasks",
-     image: "/images/BlogImages/mask 1.png" },
+    {
+      id: 3,
+      blogTitle: "Face",
+      title: "Мasks",
+      image: "/images/BlogImages/mask 1.png",
+    },
   ],
   [
     {
       id: 1,
-      blogTitle:'Body',
+      blogTitle: "Body",
       title: "BodyScrab",
       image: "/images/BlogImages/Body-Scrab 1.png",
     },
     {
       id: 2,
-      blogTitle:'Body',
+      blogTitle: "Body",
       title: "BodyLotion",
       image: "/images/BlogImages/Body-lotion.png",
     },
-    { id: 3, 
-      blogTitle:'Body',
+    {
+      id: 3,
+      blogTitle: "Body",
       title: "BodyOil",
-     image: "/images/BlogImages/Body-oil.png" },
+      image: "/images/BlogImages/Body-oil.png",
+    },
   ],
   [
     {
       id: 1,
-      blogTitle:'Hair',
+      blogTitle: "Hair",
       title: "Split Ends",
       image: "/images/BlogImages/hairbrush-box.png",
     },
     {
       id: 2,
-      blogTitle:'Hair',
+      blogTitle: "Hair",
       title: "Hair Styling",
       image: "/images/BlogImages/Hair-Styling.png",
     },
     {
       id: 3,
-      blogTitle:'Hair',
+      blogTitle: "Hair",
       title: "Hairbrush",
       image: "/images/BlogImages/Hair-split ends.png",
     },
   ],
 ];
 
-export const blogTextMain=[
+export const blogTextMain = [
   [
-    { id: 1, 
-      title: "Face",
-       image: "/images/BlogImages/acne.png" },
-    {
-      id: 2,
-      title: "Body",
-      image: "/images/BlogImages/clay-mask.png",
-    },
-    { id: 3,
-       title: "Hair",
-     image: "/images/BlogImages/freckles.png" },
+    { id: 1, title: "Face", image: "/images/BlogImages/acne.png" },
+    { id: 2, title: "Body", image: "/images/BlogImages/clay-mask.png" },
+    { id: 3, title: "Hair", image: "/images/BlogImages/freckles.png" },
   ],
-]
+];
