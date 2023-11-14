eb1a86766338ab66ca188b8e695dbd7a9e379d6a
Piko327 <fikii.business@gmail.com>
2023-11-14 14:38:27 +0200

    download errorImage

--- bellebloom/src/app/components/Card/Card.jsx
+++ bellebloom/src/app/components/Card/Card.jsx
@@ -1,16 +1,14 @@
 import Image from "next/image";
 import React from "react";
+import { redirect } from 'next/navigation'
 
-const Card = ({ product, setLoading }) => {
+const Card = ({ product}) => {
   return (
-    <li key={product.id} props={product} className="card">
+    <li key={product.id} props={product} className="card" >
       <Image
         onError={(e) => {
-          setLoading(true);
-          e.target.onerror = null; // prevent infinite loop
-          e.target.src =
-            "https://scontent.fpdv1-1.fna.fbcdn.net/v/t1.6435-9/118086965_3083762088359603_978286998271727299_n.jpg?_nc_cat=106&ccb=1-7&_nc_sid=c2f564&_nc_ohc=nY2jrGP7TJMAX887GN_&_nc_ht=scontent.fpdv1-1.fna&oh=00_AfCcSyHMFEBnwyFzKXV3xiXxH_9QPzelPA27s-tlf7bN7g&oe=657A0337"; // replace with your placeholder image
-          setLoading(false);
+          e.target.onerror = null; 
+          e.target.src = './images/errorImage.jpg';
         }}
         priority
         width={200}
