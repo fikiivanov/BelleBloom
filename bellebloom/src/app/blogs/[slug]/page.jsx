4a7852a29ae0e4f4d74b3553bcad13b0a9f18e76
Piko327 <fikii.business@gmail.com>
2023-11-16 15:32:26 +0200

    Update page.jsx

--- bellebloom/src/app/blogs/[slug]/page.jsx
+++ bellebloom/src/app/blogs/[slug]/page.jsx
@@ -1,6 +1,26 @@
 "use client";
 import Blog from "@/app/components/Blog/Blog";
 import React, {  } from "react";
+
+export async function getStaticPaths() {
+  return {
+    paths: [
+      {
+        params: {
+         slug:"face"
+        },
+        params: {
+          slug:"hair"
+         },
+         params: {
+          slug:"body"
+         },
+      }, // See the "paths" section below
+    ],
+    fallback: true, // false or "blocking"
+  }
+}
+ 
 const page = ({params}) => {
 
 
