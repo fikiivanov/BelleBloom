8a16da4f5ac5c0a9b65d29bd2106699cec8ea30a
Piko327 <fikii.business@gmail.com>
2023-11-22 14:02:39 +0200

    add Header and Footer in product page

--- bellebloom/src/app/products/[id]/page.jsx
+++ bellebloom/src/app/products/[id]/page.jsx
@@ -1,16 +1,25 @@
+import Footer from "@/app/components/Footer/Footer";
+import Header from "@/app/components/Header/Header";
 
 export async function generateStaticParams() {
-    const posts = await fetch(
-      "https://makeup-api.herokuapp.com/api/v1/products.json"
-    ).then((res) => res.json());
-  
-    return posts.map((post) => {
-      return { id: `${post.id}` };
-    });
+  const posts = await fetch(
+    "https://makeup-api.herokuapp.com/api/v1/products.json"
+  ).then((res) => res.json());
+
+  return posts.map((post) => {
+    return { id: `${post.id}` };
+  });
 }
 
-
 const page = ({ params }) => {
-  return <div>{params.id} </div>;
+  console.log(params);
+  return (
+    <div>
+      <Header caller={"header"} />
+      {params.id}
+
+      <Footer />
+    </div>
+  );
 };
 export default page;
