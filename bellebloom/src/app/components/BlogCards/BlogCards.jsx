86fade3cd066863d268710c1a8365b189a1f6700
Piko327 <fikii.business@gmail.com>
2023-12-07 14:18:02 +0200

    add condition to BlogCards to redirect to blogs

--- bellebloom/src/app/components/BlogCards/BlogCards.jsx
+++ bellebloom/src/app/components/BlogCards/BlogCards.jsx
@@ -8,57 +8,61 @@ import Link from "next/link";
 import { container, framerVariantTitle, item } from "./blogCardAnimations";
 
 const BlogCards = ({ props }) => {
-  let link = `blogs/${props[0].blogTitle}`;
 
-  return (
-    <section className="section-blog-cards">
-      <div className="shell">
-        <div className="section__inner">
-          <ShowEffectProvider variant={framerVariantTitle}>
-            <h2>{props[0].blogTitle}</h2>
-          </ShowEffectProvider>
-          <motion.ul
-            initial="hidden"
-            whileInView="show"
-            variants={container}
-            className="blog-cards"
-          >
-            {props.map((card) => (
-              <motion.li variants={item} key={card.id} className="blog-card">
-                <Image width={400} height={400} src={card.image} alt="" />
+    console.log(props)
+    let link = `blogs/${props[0].blogTitle}`;
 
-                <div className="card__text">
-                  <h3>{card.title}</h3>
+    return (
+        <section className="section-blog-cards">
+            <div className="shell">
+                <div className="section__inner">
+                    <ShowEffectProvider variant={framerVariantTitle}>
+                        <h2>{props[0].blogTitle}</h2>
+                    </ShowEffectProvider>
+                    <motion.ul
+                        initial="hidden"
+                        whileInView="show"
+                        variants={container}
+                        className="blog-cards"
+                    >
+                        {props.map((card) => (
+                            <motion.li variants={item} key={card.id} className="blog-card">
+                                <Image width={400} height={400} src={card.image} alt="" />
 
-                  <Link
-                    href={
-                      link == "blogs/Cosmetic Info"
-                        ? `/blogs/${card.title.toLowerCase()}`
-                        : `${link.toLowerCase().replace(/\s/g, "")}#${card.title
-                            .toLowerCase()
-                            .replace(/\s/g, "")}`
-                    }
-                  >
-                    More
-                  </Link>
+                                <div className="card__text">
+                                    <h3>{card.title}</h3>
+
+                                    <Link
+                                        href={
+                                            link == "blogs/Cosmetic Info"
+                                                ? `/blogs/${card.title.toLowerCase()}`
+                                                : `${link.toLowerCase().replace(/\s/g, "")}#${card.title
+                                                    .toLowerCase()
+                                                    .replace(/\s/g, "")}`
+                                        }
+                                    >
+                                        More
+                                    </Link>
+                                </div>
+                            </motion.li>
+                        ))}
+                    </motion.ul>
+                    <Link
+                        href={
+                            props[0].blogTitle == "Face" && "Body" && "Hair"  ? `blogs/${props[0].blogTitle
+                                .toLowerCase()
+                                .replace(/\s/g, "")}`
+                                : `blogs/`}
+                        className="btn btn--reverse "
+                    >
+                        <span>More </span>
+                    </Link>
                 </div>
-              </motion.li>
-            ))}
-          </motion.ul>
-          <Link
-            href={`blogs/${props[0].blogTitle
-              .toLowerCase()
-              .replace(/\s/g, "")}`}
-            className="btn btn--reverse "
-          >
-            <span>More </span>
-          </Link>
-        </div>
 
-        <hr className="line" />
-      </div>
-    </section>
-  );
+                <hr className="line" />
+            </div>
+        </section>
+    );
 };
 
 export default BlogCards;
